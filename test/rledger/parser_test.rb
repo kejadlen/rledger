a38b4436f4b137c81f4a288e90c51eed39f3fd50
Alpha Chen <alpha@kejadlen.dev>
2021-10-01 20:29:46 -0700

    use parse_with_debug in tests

--- test/rledger/parser_test.rb
+++ test/rledger/parser_test.rb
@@ -2,6 +2,8 @@
 
 require "test_helper"
 
+require "parslet/convenience"
+
 class ParserTest < Minitest::Test
   make_my_diffs_pretty!
 
@@ -23,19 +25,19 @@ class ParserTest < Minitest::Test
   def test_journal
     journal = ([ "commodity $1000.00\n" ] + TRANSACTIONS).join("\n")
 
-    Parser.new.journal.parse(journal)
-    Parser.new.parse(journal)
+    Parser.new.journal.parse_with_debug(journal)
+    Parser.new.parse_with_debug(journal)
   end
 
   def test_transaction
-    parsed = Parser.new.transaction.parse(TRANSACTIONS[0])
+    parsed = Parser.new.transaction.parse_with_debug(TRANSACTIONS[0])
     assert_equal [
       { date: "2008/01/01", description: "income" },
       { posting: { account_name: "assets:bank:checking", amount: "$1" } },
       { posting: { account_name: "income:salary", amount: "$-1" } },
     ], parsed
 
-    parsed = Parser.new.transaction.parse(TRANSACTIONS[1])
+    parsed = Parser.new.transaction.parse_with_debug(TRANSACTIONS[1])
     assert_equal [
       { date: "2012/5/14", description: "something", comment: "a transaction comment" },
       { comment: "the transaction comment, continued" },
@@ -47,24 +49,24 @@ class ParserTest < Minitest::Test
   end
 
   def test_simple_date
-    Parser.new.simple_date.parse("2010-01-31")
-    Parser.new.simple_date.parse("2010/01/31")
-    Parser.new.simple_date.parse("2010.01.31")
-    Parser.new.simple_date.parse("1/31")
-    Parser.new.simple_date.parse("2012/5/14")
+    Parser.new.simple_date.parse_with_debug("2010-01-31")
+    Parser.new.simple_date.parse_with_debug("2010/01/31")
+    Parser.new.simple_date.parse_with_debug("2010.01.31")
+    Parser.new.simple_date.parse_with_debug("1/31")
+    Parser.new.simple_date.parse_with_debug("2012/5/14")
   end
 
   def test_posting
-    parsed = Parser.new.posting.parse("assets:bank:checking   $1\n")
+    parsed = Parser.new.posting.parse_with_debug("assets:bank:checking   $1\n")
     assert_equal "assets:bank:checking", parsed[:account_name]
     assert_equal "$1", parsed[:amount]
 
-    parsed = Parser.new.posting.parse("income:salary         $-1\n")
+    parsed = Parser.new.posting.parse_with_debug("income:salary         $-1\n")
     assert_equal "income:salary", parsed[:account_name]
     assert_equal "$-1", parsed[:amount]
   end
 
   def test_commodity
-    Parser.new.commodity.parse("commodity $1000.00\n")
+    Parser.new.commodity.parse_with_debug("commodity $1000.00\n")
   end
 end
